82f50178aa598b8f8a8cb4ea1438379b3c132540
Oleksiy Martynov <oleksiy@alwaysaugust.com>
2021-03-17 17:41:50 -0400

    fixes!

--- pages/api/lightql.js
+++ pages/api/lightql.js
@@ -4,39 +4,18 @@ import graphqlSchema from "../../models";
 
 export default async function handler(req, res) {
   try {
-
+    await connect(process.env.MONGO_DB_URL);//IMPORTANT! always disconnect() or production serverless will stall
     console.log(`Connected to ${process.env.MONGO_DB_URL}`);
-    await connect(process.env.MONGO_DB_URL);
-
-    console.log(`Disconnected to ${process.env.MONGO_DB_URL}`);
-    await disconnect();
-    res.status(200).json({ data: "hello" });
-    // try{
-    //     const result = await promisify(graphql(graphqlSchema, JSON.parse(req.body).query));
-    //     console.log("got result:"+result);
-    //     await mongoose.disconnect();
-    //     res.status(200).json({ data: result });
-    //     return {
-    //         statusCode: 200,
-    //         headers: {
-    //           'Content-Type': 'application/json',
-    //         },
-    //         body: JSON.stringify({ data: result }),
-    //       };
-    // }catch(err){
-    //     console.log("got error")
-    //     console.log(err);
-    //     res.status(500).json({ error: "GraphQL_Error" });
-    //     return {
-    //         statusCode: 500,
-    //         headers: {
-    //           'Content-Type': 'application/json',
-    //         },
-    //         body: JSON.stringify({ error: "GraphQL_Error" }),
-    //       };
-    // }
+    try{
+        const result = await promisify(graphql(graphqlSchema, JSON.parse(req.body).query));
+        await disconnect(); 
+        res.status(200).json({ data: result });
+    }catch(err){
+        console.log(err);
+        await disconnect(); 
+        res.status(500).json({ error: "GraphQL_Error" });
+    }
   } catch (err) {
-    console.log(err);
     res.status(500).json({ error: "DB_Error" });
     return {
         statusCode: 500,
