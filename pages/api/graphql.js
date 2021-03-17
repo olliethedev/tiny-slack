79ff7c798ad0a69d40690b8953132cc2b0e880e5
Oleksiy Martynov <oleksiy@alwaysaugust.com>
2021-03-17 15:12:51 -0400

    logs

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -2,9 +2,12 @@ import { ApolloServer } from "apollo-server-micro";
 import graphqlSchema  from '../../models';
 import { connect } from '../../utils/database';
 
+console.log('Loaded code');
 export default new ApolloServer({ schema: graphqlSchema,
   context: async () => {
+    console.log('before connection');
     const db = await connect(process.env.MONGO_DB_URL);
+    console.log('after connection');
     return {db};
   } }).createHandler({
   path: "/api/graphql",
