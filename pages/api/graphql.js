57f00a788ea41f0624beaaa8fad9bfb9a91eb3fd
Oleksiy Martynov <oleksiy@alwaysaugust.com>
2021-03-17 18:12:21 -0400

    playground fix

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,17 +1,23 @@
 import { ApolloServer } from "apollo-server-micro";
 import graphqlSchema  from '../../models';
-import { connect } from '../../utils/database';
+import { connect, disconnect } from '../../utils/database';
 
 console.log('Loaded code');
-export default new ApolloServer({ schema: graphqlSchema,
-  context: async () => {
-    console.log('before connection');
-    const db = await connect(process.env.MONGO_DB_URL);
-    console.log('after connection');
-    return {db};
-  } }).createHandler({
-  path: "/api/graphql",
-});
+export default async function handler(req, res){
+  const apolloHandler = new ApolloServer({ schema: graphqlSchema,
+    context: async () => {
+      console.log('before connection');
+      const db = await connect(process.env.MONGO_DB_URL);
+      console.log('after connection');
+      return {db};
+    } }).createHandler({
+    path: "/api/graphql",
+  })
+  await apolloHandler(req, res);
+  console.log('handler called')
+  await disconnect();
+  console.log('disconnected')
+};
 export const config = {
   api: {
     bodyParser: false,
